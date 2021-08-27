5868effc65ee4b2db4e15783e0fff63d8fdebe78
Keelan Burnham <keelanburnham@Keelans-MacBook-Pro.local>
2021-08-27 08:20:03 -0500

    add task to list

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const containerTaskList = document.querySelector('.task-list');
+const inputTask = document.querySelector('input');
+const tasks = [];
+
+inputTask.addEventListener('keydown', function (e) {
+  if (e.code === 'Enter') {
+    AddTask();
+  }
+});
+
+const AddTask = function () {
+  let task = document.querySelector('input').value;
+  tasks.push(task);
+  UpdateTaskList(tasks);
+};
+
+const UpdateTaskList = function (tasks) {
+  containerTaskList.innerHTML = '';
+  tasks.forEach(function (task, index) {
+    const html = `
+    <li class="task">
+      <input type="checkbox" name="task-${index + 1}" id="task-${index + 1}" />
+      <label for="task-${index + 1}">${task}</label>
+    </li>
+    `;
+    containerTaskList.insertAdjacentHTML('afterbegin', html);
+  });
+};
